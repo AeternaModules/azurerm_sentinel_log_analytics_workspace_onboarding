0e1bde0e7fd01c4df02559179f2142988ab20a14
Daniel Allen <dan@aeternagroup.net>
2026-01-11 18:06:39 +0000

    Update to schema version v4.57.0

---
+++ outputs.tf
@@ -0,0 +1,5 @@
+output "sentinel_log_analytics_workspace_onboardings" {
+  description = "All sentinel_log_analytics_workspace_onboarding resources"
+  value       = azurerm_sentinel_log_analytics_workspace_onboarding.sentinel_log_analytics_workspace_onboardings
+}
+
