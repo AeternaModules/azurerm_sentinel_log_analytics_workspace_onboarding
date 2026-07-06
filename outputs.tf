2fb163b54f4c963f3ded92040c084375655a8ef5
Daniel Allen <dan@aeternagroup.net>
2026-07-06 23:14:59 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -2,4 +2,12 @@ output "sentinel_log_analytics_workspace_onboardings" {
   description = "All sentinel_log_analytics_workspace_onboarding resources"
   value       = azurerm_sentinel_log_analytics_workspace_onboarding.sentinel_log_analytics_workspace_onboardings
 }
+output "sentinel_log_analytics_workspace_onboardings_customer_managed_key_enabled" {
+  description = "List of customer_managed_key_enabled values across all sentinel_log_analytics_workspace_onboardings"
+  value       = [for k, v in azurerm_sentinel_log_analytics_workspace_onboarding.sentinel_log_analytics_workspace_onboardings : v.customer_managed_key_enabled]
+}
+output "sentinel_log_analytics_workspace_onboardings_workspace_id" {
+  description = "List of workspace_id values across all sentinel_log_analytics_workspace_onboardings"
+  value       = [for k, v in azurerm_sentinel_log_analytics_workspace_onboarding.sentinel_log_analytics_workspace_onboardings : v.workspace_id]
+}
 
