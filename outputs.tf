fb6fae74b750f41f5bd1e7c19930dc3f4ddabc63
Daniel Allen <dan@aeternagroup.net>
2026-07-12 16:04:49 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
+output "sentinel_log_analytics_workspace_onboardings_id" {
+  description = "Map of id values across all sentinel_log_analytics_workspace_onboardings, keyed the same as var.sentinel_log_analytics_workspace_onboardings"
+  value       = { for k, v in azurerm_sentinel_log_analytics_workspace_onboarding.sentinel_log_analytics_workspace_onboardings : k => v.id }
+}
 output "sentinel_log_analytics_workspace_onboardings_customer_managed_key_enabled" {
   description = "Map of customer_managed_key_enabled values across all sentinel_log_analytics_workspace_onboardings, keyed the same as var.sentinel_log_analytics_workspace_onboardings"
   value       = { for k, v in azurerm_sentinel_log_analytics_workspace_onboarding.sentinel_log_analytics_workspace_onboardings : k => v.customer_managed_key_enabled }
