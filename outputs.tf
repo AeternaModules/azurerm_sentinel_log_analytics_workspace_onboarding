563da826324dbfe5b3c07e6f0f8ed9077321dbeb
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:20:16 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,13 +1,9 @@
-output "sentinel_log_analytics_workspace_onboardings" {
-  description = "All sentinel_log_analytics_workspace_onboarding resources"
-  value       = azurerm_sentinel_log_analytics_workspace_onboarding.sentinel_log_analytics_workspace_onboardings
-}
 output "sentinel_log_analytics_workspace_onboardings_customer_managed_key_enabled" {
-  description = "List of customer_managed_key_enabled values across all sentinel_log_analytics_workspace_onboardings"
-  value       = [for k, v in azurerm_sentinel_log_analytics_workspace_onboarding.sentinel_log_analytics_workspace_onboardings : v.customer_managed_key_enabled]
+  description = "Map of customer_managed_key_enabled values across all sentinel_log_analytics_workspace_onboardings, keyed the same as var.sentinel_log_analytics_workspace_onboardings"
+  value       = { for k, v in azurerm_sentinel_log_analytics_workspace_onboarding.sentinel_log_analytics_workspace_onboardings : k => v.customer_managed_key_enabled }
 }
 output "sentinel_log_analytics_workspace_onboardings_workspace_id" {
-  description = "List of workspace_id values across all sentinel_log_analytics_workspace_onboardings"
-  value       = [for k, v in azurerm_sentinel_log_analytics_workspace_onboarding.sentinel_log_analytics_workspace_onboardings : v.workspace_id]
+  description = "Map of workspace_id values across all sentinel_log_analytics_workspace_onboardings, keyed the same as var.sentinel_log_analytics_workspace_onboardings"
+  value       = { for k, v in azurerm_sentinel_log_analytics_workspace_onboarding.sentinel_log_analytics_workspace_onboardings : k => v.workspace_id }
 }
 
